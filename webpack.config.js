e2e5bf934510f340d40654747a41a7e631591449
wwk <1198949718@qq.com>
2020-12-21 22:07:30 +0800

    配置webpack

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+module.exports = {
+  entry: './src/main.js',
+  output:{
+    filename:'bundle.js',
+    path:path.resolve(__dirname,'dist')
+  },
+  module:{
+    rules:[
+      {
+        test:/\.js$/,
+        exclude:/node_modules/,
+        use:{
+          loader:'babel-loader'
+        }
+      },
+      {
+        test:/\.html$/,
+        use:{
+          loader:'html-loader'
+        }
+      }
+    ]
+  },
+  plugins:[
+    new HtmlWebPackPlugin({
+      title:"seaside_cottage",
+      filename:'index.html',
+      template:'./src/index.html'
+    })
+  ],
+  devtool:'inline-source-map',
+}
